6887e32e1fae517a5033e1323cd3abb15c2ce7f0
Adam Milo <amilo@ultralig.ht>
2022-11-08 04:53:37 -0500

    added new view for adding address

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Adam Gerber on 08/11/2022.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView()
+    }
+}
