fe7721c77c102bc9502560cff84089dd7e81f722
Adam Milo <amilo@ultralig.ht>
2022-11-08 04:54:43 -0500

    added new view for checkout

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    
+    @ObservedObject var order: Order
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -15,6 +18,6 @@ struct CheckoutView: View {
 
 struct CheckoutView_Previews: PreviewProvider {
     static var previews: some View {
-        CheckoutView()
+        CheckoutView(order: Order())
     }
 }
